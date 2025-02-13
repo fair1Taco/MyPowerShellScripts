1a15048029e94988adee5245679cb1ff5ebe1436
fair1Taco <daniel.berg1991@gmail.com>
2025-02-13 18:08:40 +0100

    Add files via upload

--- Entra-NewUser.ps1
+++ Entra-NewUser.ps1
@@ -1,3 +1,4 @@
+#Parameters for the script
 param (
     [Parameter(Mandatory = $true)][String]$DisplayName,
     [Parameter(Mandatory = $true)][String]$UserPrincipalName,
@@ -5,28 +6,45 @@ param (
     [Parameter(Mandatory = $false)][String]$Group
 )  
 
-Connect-MgGraph -scopes "user.readwrite.all, group.readwrite.all"
-
-$PWProfile = @{
-    Password = $PWD;
-    ForceChangePasswordNextSignIn = $false
+#Installs microsoft.graph if already not installed
+if  (-not (Get-Module -listAvailable -Name Microsoft.Graph)) {
+    Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery -Force
+    Write-Warning "Microsoft Graph module is not installed. Installing..."
 }
 
-$MailNickname = $UserPrincipalName.Split('@')[0]
+#Connects user to microsoft.graph
+Connect-MgGraph -scopes "user.readwrite.all, group.readwrite.all" -NoWelcome
+
+#Adds new user to Microsoft Entra
+try {
+    $PWProfile = @{
+        Password = $PWD;
+        ForceChangePasswordNextSignIn = $false
+    }
+    $MailNickname = $UserPrincipalName.Split('@')[0]
 
     New-MgUser `
         -DisplayName $DisplayName `
         -MailNickname $MailNickname `
         -UserPrincipalName $UserPrincipalName `
         -PasswordProfile $PWProfile -AccountEnabled `
+}
+catch {
+    Read-Host "Unexpected error, $DisplayName not added to Microsoft Entra"
+}
 
-    $Addtogroup = Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}
-    $Addthisuser = Get-MgUser | Where-Object {$_.DisplayName -eq "$DisplayName"}
 
-    if (-not $Group) {
-        $Group = Read-Host "User not assigned to a group" -AsSecureString
+#Assigns the user to group
+if (-not $Group) {
+        $Group = Read-Host "User added without group assignment" -AsSecureString
+}
+else { try {
+        $Addtogroup = Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}
+        $Addthisuser = Get-MgUser | Where-Object {$_.DisplayName -eq "$DisplayName"}
+        New-MgGroupMember -GroupId $Addtogroup.Id -DirectoryObjectId $Addthisuser.Id  
     }
-    else {
-    New-MgGroupMember -GroupId $Addtogroup.Id -DirectoryObjectId $Addthisuser.Id
-    }
-
+catch {
+        Read-Host "Unexpected error. User added, but without group assignment"
+    } 
+   
+}
